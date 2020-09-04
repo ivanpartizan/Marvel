459619b82054816db5831248fdddb550030440f0
Ivan Milenkovic <argentinaivan@hotmail.com>
2020-09-04 22:47:06 +0200

    update

--- script.js
+++ script.js
@@ -1,46 +1,84 @@
-// const apiUrls = [
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=0",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=100",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=200",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=300",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=400",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=500",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=600",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=700",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=800",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=900",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1000",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1100",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1200",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1300",
-//   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1400",
-// ];
+const apiUrls = [
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=0",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=100",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=200",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=300",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=400",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=500",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=600",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=700",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=800",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=900",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1000",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1100",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1200",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1300",
+  "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1400",
+];
 
-// async function getData() {
-//   const response = await Promise.all(apiUrls.map((url) => fetch(url)));
-//   const data = await Promise.all(response.map((res) => res.json()));
-//   console.log(data);
-//   for (line of data) {
-//     const character = line.data.results;
-//     console.log(character);
-//     for (hero of character) {
-//       const heroo = hero.name;
-//       console.log(heroo);
-//     }
-//   }
-//   // const characters = data[i].data.results;
-//   // console.log(characters);
-//   // for (character of characters) {
-//   //   console.log(character.name);
-// }
-// // const response = await fetch(apiUrl);
-// // const data = await response.json();
-// // console.log(data);
-// // console.log(data.attributionText);
-// // const characters = data.data.results;
-// // console.log(characters);
-// // for (character of characters) {
-// //   console.log(character.name);
-// // }
+async function getData() {
+  const response = await Promise.all(apiUrls.map((url) => fetch(url)));
+  const data = await Promise.all(response.map((res) => res.json()));
+  console.log(data);
+  // for (part of data) {
+  //   const character = part.data.results;
+  //   console.log(character);
+  //   for (part of character) {
+  //     const name = part.name;
+  //     console.log(name);
+  //   }
+  // }
+  for (item of data) {
+    const part = item.data.results;
+    console.log(part);
+    for (character of part)
+      document.getElementById("output").innerHTML += `
+      <div class="character">
+        <h1>${character.name}</h1>
+        <a href="${character.urls[0].url}" target="_blank">
+          <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}" height="100" class="image" />
+        </a>
+      </div>
+  `;
+  }
+}
 
-// getData();
+// const displayData = (data) => {
+//   document.getElementById("output").innerHTML = `${character
+//     .map(
+//       (hero) =>
+//         `
+//   <h1>${hero.name}</h1>
+//   <a href="${hero.urls[0].url}" target="_blank">
+//   <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" height='100' class='image' />
+//   </a>
+//   `
+//     )
+//     .join("")}`;
+// output.innerHTML = content;
+// };
+
+getData();
+
+const heroes = ["Spider-Man", "Iron Man", "Hulk", "Thor", "Captain America"];
+let hero = 0;
+let letter = 0;
+let currentHero = "";
+let currentLetter = "";
+
+(function typing() {
+  if (hero === heroes.length) {
+    hero = 0;
+  }
+  currentHero = heroes[hero];
+
+  currentLetter = currentHero.slice(0, ++letter);
+
+  document.querySelector("#heroes").textContent = currentLetter;
+
+  if (currentLetter.length === currentHero.length) {
+    hero++;
+    letter = 0;
+  }
+  setTimeout(typing, 250);
+})();
