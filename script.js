329fa562adccd2f8a1efa58077db4c7aafb2314b
Ivan Milenkovic <argentinaivan@hotmail.com>
2020-09-05 05:01:02 +0200

    update

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ const apiUrls = [
   "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=88f6ef315f2d376003352ecb882ae5bd&hash=044cf0f1a4329df3001d5f95895141ac&limit=100&offset=1400",
 ];
 
+let data = [];
+
 async function getData() {
   const response = await Promise.all(apiUrls.map((url) => fetch(url)));
-  const data = await Promise.all(response.map((res) => res.json()));
+  data = await Promise.all(response.map((res) => res.json()));
   console.log(data);
   // for (part of data) {
   //   const character = part.data.results;
@@ -31,18 +33,38 @@ async function getData() {
   for (item of data) {
     const part = item.data.results;
     console.log(part);
-    for (character of part)
-      document.getElementById("output").innerHTML += `
-      <div class="character">
-        <h1>${character.name}</h1>
-        <a href="${character.urls[0].url}" target="_blank">
-          <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}" height="100" class="image" />
-        </a>
-      </div>
-  `;
+    // displayData(data);
+    for (character of part) {
+      const displayData = (data) => {
+        document.getElementById("output").innerHTML += `
+          <div class="character">
+            <h1>${character.name}</h1>
+            <a href="${character.urls[0].url}" target="_blank">
+              <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}" height="100" class="image" />
+            </a>
+          </div>
+      `;
+      };
+
+      const searchBar = document.querySelector("#searchbar");
+      searchBar.addEventListener("keyup", (e) => {
+        const searchString = e.target.value.toLowerCase();
+        const filter = data.filter((character) =>
+          character.name.toLowerCase().includes(searchString)
+        );
+        displayData(filter);
+        //     document.getElementById("output").innerHTML += `
+        //     <div class="character">
+        //       <h1>${character.name}</h1>
+        //       <a href="${character.urls[0].url}" target="_blank">
+        //         <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}" height="100" class="image" />
+        //       </a>
+        //     </div>
+        // `;
+      });
+    }
   }
 }
-
 // const displayData = (data) => {
 //   document.getElementById("output").innerHTML = `${character
 //     .map(
@@ -59,6 +81,26 @@ async function getData() {
 // };
 
 getData();
+
+// const displayData = (data) => {
+//   document.getElementById("output").innerHTML += `
+//       <div class="character">
+//         <h1>${character.name}</h1>
+//         <a href="${character.urls[0].url}" target="_blank">
+//           <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}" height="100" class="image" />
+//         </a>
+//       </div>
+//   `;
+// };
+
+// const searchBar = document.querySelector("#searchbar");
+// searchBar.addEventListener("keyup", (e) => {
+//   const searchString = e.target.value.toLowerCase();
+//   const filter = data.filter((character) =>
+//     character.name.toLowerCase().includes(searchString)
+//   );
+//   displayData(filter);
+// };
 
 const heroes = ["Spider-Man", "Iron Man", "Hulk", "Thor", "Captain America"];
 let hero = 0;
